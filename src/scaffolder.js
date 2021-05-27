bc44292827490af115081dc6f5b89412b654322b
Matt Travi <programmer@travi.org>
2021-05-26 23:14:14 -0500

    feat(slides): included the initial framework config in the scaffolded deck

--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,7 +1,29 @@
 import {promises as fs} from 'fs';
 
 export default async function ({projectRoot}) {
-  await fs.writeFile(`${projectRoot}/slides.md`, '');
+  await fs.writeFile(
+    `${projectRoot}/slides.md`,
+    `---
+theme: default
+
+# random image from a curated Unsplash collection by Anthony
+# like them? see https://unsplash.com/collections/94734566/slidev
+background: https://source.unsplash.com/collection/94734566/1920x1080
+
+# apply any windi css classes to the current slide
+class: 'text-center'
+
+# https://sli.dev/custom/highlighters.html
+highlighter: prism
+
+# some information about the slides, markdown enabled
+info: |
+  ## Slidev Starter Template
+  Presentation slides for developers.
+
+  Learn more at [Sli.dev](https://sli.dev)
+---`
+  );
 
   return {
     dependencies: ['@slidev/cli', '@slidev/theme-default'],
--- test/integration/features/step_definitions/scaffold-steps.js
+++ test/integration/features/step_definitions/scaffold-steps.js
@@ -1,9 +1,31 @@
+import {promises as fs} from 'fs';
 import {Then} from '@cucumber/cucumber';
 import {assert} from 'chai';
-import {fileExists} from '@form8ion/core';
 
 Then('the expected files are generated', async function () {
-  assert.isTrue(await fileExists(`${process.cwd()}/slides.md`));
+  assert.equal(
+    await fs.readFile(`${process.cwd()}/slides.md`, 'utf-8'),
+    `---
+theme: default
+
+# random image from a curated Unsplash collection by Anthony
+# like them? see https://unsplash.com/collections/94734566/slidev
+background: https://source.unsplash.com/collection/94734566/1920x1080
+
+# apply any windi css classes to the current slide
+class: 'text-center'
+
+# https://sli.dev/custom/highlighters.html
+highlighter: prism
+
+# some information about the slides, markdown enabled
+info: |
+  ## Slidev Starter Template
+  Presentation slides for developers.
+
+  Learn more at [Sli.dev](https://sli.dev)
+---`
+  );
 });
 
 Then('the scripts are defined', async function () {
