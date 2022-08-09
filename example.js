08929656cf2c357ef7130778f3c42d4568964f2f
Matt Travi <programmer@travi.org>
2022-08-08 23:02:55 -0500

    docs(example): stubbed node modules

--- example.js
+++ example.js
@@ -1,10 +1,11 @@
 // #### Import
-// remark-usage-ignore-next
+// remark-usage-ignore-next 2
+import {resolve} from 'path';
 import stubbedFs from 'mock-fs';
 import {scaffold} from './lib/index.cjs';
 
 // remark-usage-ignore-next
-stubbedFs();
+stubbedFs({node_modules: stubbedFs.load(resolve(__dirname, 'node_modules'))});
 
 // #### Execute
 
